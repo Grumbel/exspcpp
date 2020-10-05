05339731ef87d307ffaf0c0ec596ea10d3de9ce2
Ingo Ruhnke <grumbel@gmail.com>
2020-10-05 17:00:04 +0200

    Add exsp::find_exe()

---
+++ src/path.cpp
@@ -0,0 +1,55 @@
+// exsp - Spawning subprocesses in C++
+// Copyright (C) 2008-2020 Ingo Ruhnke <grumbel@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include <filesystem>
+#include <optional>
+
+#include <string.h>
+
+namespace exsp {
+
+std::optional<std::filesystem::path>
+find_exe(std::string_view name)
+{
+  char* path_c = getenv("PATH");
+  if (!path_c) {
+    return {};
+  } else {
+    const char* delim = ":";
+    char* path = strdup(path_c);
+    char* state;
+    for(char* p = strtok_r(path, delim, &state); p != nullptr; p = strtok_r(nullptr, delim, &state)) {
+      std::filesystem::path exepath(p);
+      exepath /= name;
+      try {
+        auto perms = std::filesystem::status(exepath).permissions();
+        if ((perms & std::filesystem::perms::owner_exec) != std::filesystem::perms::none) {
+          free(path);
+          return exepath;
+        }
+      } catch(...) {
+        // ignore errors
+      }
+    }
+    free(path);
+
+    return {};
+  }
+}
+
+} // namespace exsp
+
+/* EOF */
