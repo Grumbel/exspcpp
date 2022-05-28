ef0fcad51ce68e72d3ece4930fca8c4c565670c6
Ingo Ruhnke <grumbel@gmail.com>
2022-05-28 22:57:56 +0200

    Add Nix flake build file

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "exec and spawn utils for C++";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    tinycmmc.url = "gitlab:grumbel/cmake-modules";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+    tinycmmc.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, tinycmmc }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          exspcpp = pkgs.stdenv.mkDerivation {
+            pname = "exspcpp";
+            version = "0.0.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.pkg-config
+            ];
+            buildInputs = [
+              tinycmmc.defaultPackage.${system}
+            ];
+            propagatedBuildInputs = [
+              pkgs.gtest
+            ];
+           };
+        };
+        defaultPackage = packages.exspcpp;
+      });
+}
