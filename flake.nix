8e89ee2372251969a6d47109770c0af8eb9f6f56
Ingo Ruhnke <grumbel@gmail.com>
2023-03-22 15:42:58 +0100

    Update flake.nix to gcc12 and nixos-22.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "exec and spawn utils for C++";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
@@ -18,7 +18,7 @@
         packages = rec {
           default = exspcpp;
 
-          exspcpp = pkgs.stdenv.mkDerivation {
+          exspcpp = pkgs.gcc12Stdenv.mkDerivation {
             pname = "exspcpp";
             version = "0.0.0";
             src = nixpkgs.lib.cleanSource ./.;
